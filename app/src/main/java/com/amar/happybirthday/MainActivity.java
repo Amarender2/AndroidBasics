96561011affca4e7fb6cc82f7b1defd9e547d965
armekala <ammv8@mst.edu>
2018-02-25 00:06:26 -0600

    adding localization, checkboxes, intents

--- app/src/main/java/com/amar/happybirthday/MainActivity.java
+++ app/src/main/java/com/amar/happybirthday/MainActivity.java
@@ -1,9 +1,13 @@
 package com.amar.happybirthday;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -26,12 +30,47 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         int quantity = Integer.parseInt(quantityTextView.getText().toString());
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedCream);
+        boolean hasWhippedCream = whippedCream.isChecked();
+        CheckBox chocolate = (CheckBox) findViewById(R.id.choclate);
+        boolean hasChocolate = chocolate.isChecked();
+        EditText nameField = (EditText) findViewById(R.id.nameField);
+        String name = nameField.getText().toString();
+
         if(quantity > 0) {
-            priceTextView.setText(" Thank You!! Order Received of worth "
-                    + NumberFormat.getCurrencyInstance().format(price * quantity));
+            priceTextView.setText(" Thank You " + name + " !! Order Received of worth "
+                    + NumberFormat.getCurrencyInstance().format(calculatePrice(quantity, hasWhippedCream, hasChocolate))+
+                    "\n Need WhippedCream ? " + hasWhippedCream + "\n Need Chocolate ? " + hasChocolate);
         } else {
-            priceTextView.setText("Please add some coffee to your order !!!");
+            priceTextView.setText("Please add some coffee to your order !!! " + getString(R.string.toppings));
         }
+
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("geo: 47.6, -122.3"));
+//        if(intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+         intent.setType("*/*");
+//        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, "amarender.mekala@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Sample Email from android app");
+        intent.putExtra(Intent.EXTRA_TEXT, priceTextView.getText().toString());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public int calculatePrice(int quantity, boolean hasWhippedCream, boolean hasChocolate) {
+        int extraPrice = 0;
+        if(hasWhippedCream) {
+            extraPrice += 1;
+        }
+        if(hasChocolate) {
+            extraPrice += 2;
+        }
+        return (price + extraPrice) * quantity;
     }
 
     public void displayPrice(int quantity) {
